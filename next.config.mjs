14b2e403805a91ca592bb28fd1ab6354dbeae7c4
TurboJax <turbojax07@gmail.com>
2026-08-18 21:20:22 -0400

    Fixing static routing

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,8 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   output: "export",
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
   images: {
     unoptimized: true,
   },
